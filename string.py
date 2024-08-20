fc2a333280a87a37625aa86d54295bed4ad73c83
lokeshpalleda <160224924+lokeshpalleda@users.noreply.github.com>
2024-08-20 11:22:14 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,29 @@
+#string
+#wap to reverse a string
+c=0
+d=0
+s=input("enter a word:")
+print(s[::-1])
+
+rev=("")
+print("without slicing")
+for i in s:
+	rev=i+rev
+	c+=1
+print(rev)
+if rev==s:
+	print("it is palindrome")
+else:
+	print("not a palindrome")
+print("length:",c)
+print("length using len",len(rev))
+n=s.split()
+for j in n:
+	if len(j)%2==0:
+		print(j)
+m=("aeiouAEIOU")
+k=input("enter a letter:")
+if k in m:
+	print("it is a vowel")
+else:
+	print("not a vowel")
